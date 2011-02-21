3ff0a3692773d64ebbb95ffe06b9598e246f1c86
Breno G. de Oliveira <garu@cpan.org>
2011-02-21 18:51:47 +0000

    Import of GARU/Data-Printer-0.04 from CPAN.
    gitpan-cpan-distribution: Data-Printer
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         GARU/Data-Printer-0.04.tar.gz
    gitpan-cpan-author:       GARU
    gitpan-cpan-maturity:     released

--- lib/Data/Printer.pm
+++ lib/Data/Printer.pm
@@ -9,7 +9,7 @@ use Carp qw(croak);
 use Clone qw(clone);
 require Object::ID;
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 BEGIN {
     if ($^O =~ /Win32/i) {
@@ -158,7 +158,8 @@ sub _p {
     elsif ($ref eq 'Regexp') {
         my $val = "$item";
         # a regex to parse a regex. Talk about full circle :)
-        if ($val =~ m/\(\?([xismpogce]*)(?:\-[xismpogce]+)?:(.*)\)/) {
+        # note: we are not validating anything, just grabbing modifiers
+        if ($val =~ m/\(\?\^?([uladxismpogce]*)(?:\-[uladxismpogce]+)?:(.*)\)/) {
             my ($modifiers, $val) = ($1, $2);
             $string .= colored($val, $p->{color}->{'regex'});
             if ($modifiers) {
